8a37a1c80b92dbb9ed31c1178c3cdcbbbf7b4560
makkmarci13 <makkmarci03@gmail.com>
2019-08-08 13:53:19 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,11 @@ cd /home/container
 
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+usermod -u $1 container
+
+su container
+
+MODIFIED_STARTUP=`java -Xms1024M -Xmx1024M -jar spigot-1.12.2.jar --max-players 5 --port 25565 nogui`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
