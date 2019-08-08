64d0ce991c530233a230f5bf44a6533492b233e2
makkmarci13 <makkmarci03@gmail.com>
2019-08-08 13:37:06 +0200

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /home/container
+
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+${MODIFIED_STARTUP}
